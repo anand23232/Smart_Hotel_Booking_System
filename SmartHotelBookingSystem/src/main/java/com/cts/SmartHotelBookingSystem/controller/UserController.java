443f3d0cc3c2fac699e69f711798a113b3e43cf3
anand23232 <Anand.Kumar23f051@cognizant.com>
2025-04-15 11:53:14 +0530

    Added Bootstrap integration, updated booking.html, and other configurations

--- SmartHotelBookingSystem/src/main/java/com/cts/SmartHotelBookingSystem/controller/UserController.java
+++ SmartHotelBookingSystem/src/main/java/com/cts/SmartHotelBookingSystem/controller/UserController.java
@@ -1,8 +1,18 @@
 package com.cts.SmartHotelBookingSystem.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;;
 
 @Controller
 public class UserController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+    
+    @GetMapping("/booking")
+    public String booking() {
+        return "booking"; // Maps to src/main/resources/templates/booking.html
+    }
 
 }
