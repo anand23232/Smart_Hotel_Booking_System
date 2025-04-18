7966a33bf7f2e5710a8b735837b7c8aead32cbb1
anand23232 <Anand.Kumar23f051@cognizant.com>
2025-04-18 12:13:49 +0530

    controller fixed

--- SmartHotelBookingSystem/src/main/java/com/cts/SmartHotelBookingSystem/controller/UserController.java
+++ SmartHotelBookingSystem/src/main/java/com/cts/SmartHotelBookingSystem/controller/UserController.java
@@ -4,38 +4,14 @@ import com.cts.SmartHotelBookingSystem.model.User;
 import com.cts.SmartHotelBookingSystem.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-<<<<<<< HEAD
-import org.springframework.web.bind.annotation.GetMapping;
-=======
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
->>>>>>> 10862fa668bea1b802fb8da5cea5667dca5c15ca
 
 @Controller
 @RequestMapping("/users")
 public class UserController {
-<<<<<<< HEAD
-
-    @GetMapping("/login")
-    public String loginPage() {
-        return "guest/login"; // This corresponds to login.html in the templates directory
-    }
-
-    @GetMapping("/register")
-    public String registerPage() {
-        return "guest/register"; // This corresponds to signup.html in the templates directory
-    }
-
-    @GetMapping("/host")
-    public String hostPage() {
-        return "host"; // This corresponds to host.html in the templates directory
-    }
-    @GetMapping("/")
-    public String indexPage() {
-        return "index"; // This corresponds to index.html in the templates directory
-=======
     @Autowired
     private UserService userService;
 
@@ -62,6 +38,5 @@ public class UserController {
     public String deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
         return "redirect:/users";
->>>>>>> 10862fa668bea1b802fb8da5cea5667dca5c15ca
     }
 }
